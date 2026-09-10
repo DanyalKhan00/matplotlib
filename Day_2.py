62265f900bd1e350118fc10502779c0a677d27a6
DanyalKhan00 <ukt16294@email.com>
2026-09-09 21:54:08 -0700

    Day-2

---
+++ Day_2.py
@@ -0,0 +1,57 @@
+# All About Scatter Plot
+# import matplotlib.pyplot as plt
+# name = ["Ali", "Asad", "Hassan", "Jawad","ahmad","Dani","zarar","abid"]
+# marks = [45, 56, 78, 99,67,55,88,44]
+# colors = ["red","green","blue","magenta","green","red","blue","magenta"]
+# size = [120,110,100,140,120,110,140,130]
+# plt.scatter(name, marks,c=colors,s=size,alpha=0.7)#,marker="*")
+# plt.colorbar()
+# plt.title("Scatter Plot", fontsize=25)
+# plt.xlabel("Name", fontsize=25)
+# plt.ylabel("Marks", fontsize=25)
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# name = ["Ali", "Asad", "Hassan", "Jawad","ahmad","Dani","zarar","abid"]
+# marks = [45, 56, 78, 99,67,55,88,44]
+# colors=["red","green","blue","yellow","magenta","blue","yellow","red"]
+# size= [100, 150, 200, 120, 250, 180,110,130]
+# plt.scatter(name,marks,   c=colors,
+#     s=size,
+#     marker="D",
+#     alpha=0.7)
+# plt.title("Student Record",fontsize=25)
+# plt.xlabel("Student Name")
+# plt.ylabel("Student Marks")
+# plt.show()
+
+
+# import matplotlib.pyplot as plt
+
+# products = ["Laptop", "Mobile", "Tablet", "Keyboard", "Mouse"]
+# sales = [50, 80, 35, 60, 90]
+
+# plt.scatter(
+#     products,
+#     sales,
+#     color="green",
+#     s=200,
+#     marker="*",
+#     alpha=0.6
+# )
+# plt.title("Product Sales")
+# plt.xlabel("Products")
+# plt.ylabel("Sales")
+
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# students = ["Ali", "Ahmed", "Sara", "Ayesha", "Hassan", "Dani"]
+# marks = [45, 67, 88, 76, 90, 55]
+# colors = ["red", "green", "blue", "orange", "purple", "magenta"]
+# sizes = [100, 150, 200, 120, 250, 180]
+# plt.scatter(students, marks, c=colors, s=sizes,marker="D")
+# plt.title("Student Marks")
+# plt.xlabel("Students")
+# plt.ylabel("Marks")
+# plt.show()

577fd9627a7e098c3502d14de3b57681475636d3
DanyalKhan00 <ukt16294@email.com>
2026-09-10 05:55:58 -0700

    Day-3

---
+++ Day_3.py
@@ -0,0 +1,49 @@
+# All About Histogram Plot ........
+
+# import matplotlib.pyplot as plt
+# marks = [45, 55, 67, 72, 88, 91, 56, 63, 77, 82, 95, 48, 69, 74, 85]
+# plt.hist(marks , bins=15,edgecolor="black")
+# plt.title("Student Marks Distribution")
+# plt.xlabel("Marks")
+# plt.ylabel("Number of Students")
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# salary = [25000, 30000, 28000, 35000, 40000, 45000,
+#           32000, 38000, 50000, 55000, 42000, 60000]
+# plt.hist(salary,bins=6,color='black',edgecolor='yellow')
+# plt.title("Employee Salary")
+# plt.xlabel("Salary")
+# plt.ylabel("No Of Employee")
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# class_a = [45, 55, 60, 65, 70, 75, 80, 85, 90]
+# class_b = [50, 58, 62, 68, 72, 78, 82, 88, 95]
+# plt.hist(class_a,bins=6,color='black',edgecolor='yellow',alpha=0.6,label="Class-A")
+# plt.hist(class_b,bins=6,color='red',edgecolor='blue',alpha=0.6,label="Class-b")
+# plt.title("Marks Comparison")
+# plt.xlabel("Marks")
+# plt.ylabel("Student")
+# plt.legend()
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# class_a = [45, 55, 60, 65, 70, 75, 80, 85, 90,
+# 50, 58, 62, 68, 72, 78, 82, 88, 95]
+# plt.hist(class_a,bins=6,color='black',edgecolor='yellow',alpha=0.6,label="Class-A")
+# plt.title("Marks Comparison")
+# plt.xlabel("Marks")
+# plt.ylabel("Student")
+# plt.grid(axis="y", alpha=0.5)
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# scores = [35, 42, 55, 67, 78, 85, 92, 48, 63,
+#           71, 88, 95, 56, 39, 82, 76, 60]
+# bins = [0, 20, 40, 60, 80, 100]
+# plt.hist(scores, bins=bins, color="green", edgecolor="black")
+# plt.title("Exam Score Distribution")
+# plt.xlabel("Score Range")
+# plt.ylabel("Number of Students")
+# plt.show()

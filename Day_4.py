7e9d66872070891f211b518b9b16d278deea2422
DanyalKhan00 <ukt16294@email.com>
2026-09-10 06:33:39 -0700

    Day-4

---
+++ Day_4.py
@@ -0,0 +1,16 @@
+# All About Stem Plot .......
+# import matplotlib.pyplot as plt
+# x = [1,2,3,4,5,6]
+# y = [7,8,9,0,1,2]
+# plt.stem(x,y)
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# months = [1, 2, 3, 4, 5, 6]
+# sales = [120, 150, 100, 180, 200, 170]
+# plt.stem(months,sales,linefmt="-g",markerfmt="ro",basefmt="b-")
+# plt.title("Student")
+# plt.xlabel("Month")
+# plt.ylabel("Sales")
+# plt.grid(True)
+# plt.show()

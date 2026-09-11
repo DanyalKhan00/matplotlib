67ad6d20aa78ba4b4d366b7d4def571f8c057698
DanyalKhan00 <ukt16294@email.com>
2026-09-11 04:17:22 -0700

    Day-6

---
+++ Day_5.py
@@ -0,0 +1,66 @@
+# All About pie plot ...
+
+# import matplotlib.pyplot as plt
+# fruits = ["Apples", "Bananas", "Oranges"]
+# votes = [10, 6, 4]
+# plt.pie(votes, labels=fruits, autopct="%1.0f%%")
+# plt.title("Favorite Fruits Survey")
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# student = ["aimi","dani","samar","sund"]
+# marks = [156,67,89,77]
+# plt.pie(marks,labels = student,autopct="%1.1f%%")
+# plt.title("Student Marks Distribution")
+# plt.show()
+
+
+
+# import matplotlib.pyplot as plt
+# expenses = ["Food", "Rent", "Transport", "Shopping", "Bills"]
+# amount = [15000, 30000, 5000, 8000, 7000]
+# color = ["red","green","magenta","yellow","blue"]
+# plt.pie(amount,labels=expenses,colors=color,autopct="%1.1f%%")
+# plt.title("Monthly Expenses")
+# plt.show()
+
+
+
+# import matplotlib.pyplot as plt
+# fruit = ["apple","mango","banana","cheery","melon"]
+# sales = [350,400,250,500,300]
+# explode=[0,0,0.1,0,0]
+# plt.pie(sales,labels=fruit,explode=explode,autopct="%1.1f%%")
+# plt.title("Fruit Sales")
+# plt.show()
+
+# import matplotlib.pyplot as plt
+
+# languages = ["Python", "Java", "C++", "JavaScript", "C#"]
+# users = [40, 20, 15, 15, 10]
+
+# plt.pie(
+#     users,
+#     labels=languages,
+#     shadow=True
+#     startangle=90)
+# plt.title("Programming Language popuarity")
+# plt.show()
+
+# import matplotlib.pyplot as plt
+
+# departments = ["HR", "IT", "Sales", "Marketing", "Finance"]
+# employees = [15, 40, 25, 10, 10]
+
+# explode = [0, 0.1, 0, 0, 0]
+
+# colors = ["yellow", "blue", "green", "orange", "red"]
+# plt.pie(employees ,labels=departments,colors=colors,
+#     explode=explode,
+#     autopct="%1.1f%%",
+#     shadow=True,
+#     startangle=90)
+
+# plt.title("Company Employees Distribution")
+
+# plt.show()

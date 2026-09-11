771136f8f4e187c6e854a6a8654482482e105fe6
DanyalKhan00 <ukt16294@email.com>
2026-09-11 07:02:46 -0700

    Day-7

---
+++ Day_7.py
@@ -0,0 +1,32 @@
+# All About Savefig in Matplotlib.......
+
+
+# import matplotlib.pyplot as plt
+# student = ["aimi","sara","asad","jani"]
+# marks  = [12,34,56,67]
+# plt.plot(student,marks, marker='o')
+# plt.title("Student Record")
+# plt.xlabel("Student")
+# plt.ylabel("Marks")
+# plt.savefig("First.pdf",dpi=5000)
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# fruits = ["Apple", "Banana", "Mango", "Orange", "Guava"]
+# sales = [500, 300, 450, 250, 400]
+# colors = ["r","g","y","m","b"]
+# plt.scatter(fruits, sales, color=colors)
+# plt.title("Fruit Sales")
+# plt.xlabel("Fruits")
+# plt.ylabel("Sales")
+# plt.savefig("performance.png",dpi=3000)
+# plt.show()
+
+
+# import matplotlib.pyplot as plt
+# fruits = ["Apple", "Banana", "Mango", "Orange", "Guava"]
+# sales = [500, 300, 450, 250, 400]
+# plt.pie(sales, labels=fruits, autopct="%1.1f%%")
+# plt.title("Fruit Sales")
+# plt.savefig("Fruit_Sales.png")
+# plt.show()
